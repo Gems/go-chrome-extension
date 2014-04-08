f8f90c4d4e316b67abec8d5703d1005159fbd64f
Eduard Gomolyako <ed@inn.ru>
2014-04-08 20:48:44 +0400

    Add resolve caching

--- go-labelizer.js
+++ go-labelizer.js
@@ -3,30 +3,40 @@ var checkAndSetup = function() {
 		setTimeout(checkAndSetup, 50);
 		return false;
 	}
-	
+
+	var cache = {};
+
 	var $ = jQuery, PaginatorSetParametersFromJson = window.paginator.setParametersFromJson;
 
 	if (!PaginatorSetParametersFromJson) {
 		console.log("Can't find paginator method")
 		return true;
 	}
-	
+
 	$('head').append('<style type="text/css"> .pipeline-label-branch { vertical-align: middle; font-weight: normal; display: inline-block; margin: -5px 0 0 5px; } .pipeline-master { font-weight: bold; } </style>');
+
+	var putLabel = function(el, branch) {
+		el.append('<span class="pipeline-label-branch pipeline-' + branch + '" title="' + branch + '">' + (branch === 'master' ? 'master' : 'feature') + '</span>');
+	};
 
 	var labelize = function() {
 		$('.pipeline-label').each(function(idx) {
 			var el = $(this), label = el.text();
-			
-			if (!label) 
+
+			if (!label)
 				return;
-			
+
 			label = label.trim();
-			
-			try
-			{
+
+			if (!!cache[label]) {
+				putLabel(el, cache[label]);
+				return;
+			}
+
+			try {
 				var stageHref = el.parents('tr').find('#stage-detail-' + label + '-Build .detail').attr('href');
 				var consoleHref = stageHref.replace('pipelines', 'files') + '/Create_package/cruise-output/console.log';
-	
+
 				$.ajax({ url: consoleHref }).done(function(data, st, xhr) {
 					var branch = /overriding environment variable 'BRANCH' with value '([^']+)'/.exec(data);
 
@@ -34,16 +44,14 @@ var checkAndSetup = function() {
 						return;
 					}
 
-					branch = branch[1];
-					
-					el.append('<span class="pipeline-label-branch pipeline-' + branch + '" title="' + branch + '">' + (branch === 'master' ? 'master' : 'feature') + '</span>');
+					cache[label] = branch = branch[1];
+
+					putLabel(el, branch);
 				});
-			}
-			catch(e)
-			{
+			} catch(e) {
 				console.error(e);
 			}
-		});		
+		});
 	};
 
 	window.paginator.setParametersFromJson = function() {
@@ -51,9 +59,9 @@ var checkAndSetup = function() {
 
 		return PaginatorSetParametersFromJson.apply(window.paginator, arguments);
 	};
-	
+
 	labelize();
-	
+
 	return true;
 };
 
