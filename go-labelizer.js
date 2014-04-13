3aa052d559d7f6f6b02ac6cfd5f4e91de35c701f
Anton Ignatov <abietis@gmail.com>
2014-04-13 15:34:05 +0400

    Labels for branches are made links to github compare page

--- go-labelizer.js
+++ go-labelizer.js
@@ -30,27 +30,35 @@
 						$('.pipeline-label[id]', container).each(function(i, item) {
 							var $item = $(item),
 								itemData = context.data.groups[0].history[i],
-								label = $('<span>').css({
-									'display': 'inline-block',
-									'margin': '2px 0',
-									'padding': '.2em .6em .3em',
-									'font-size': '11px',
-									'font-weight': '700',
-									'line-height': '1',
-									'color': '#fff',
-									'text-align': 'center',
-									'white-space': 'nowrap',
-									'vertical-align': 'baseline',
-									'border-radius': '.25em',
-									'-webkit-font-smoothing': 'antialiased',
-									'-moz-osx-font-smoothing': 'grayscale'
-								}),
+								label = $('<a>')
+									.css({
+										'display': 'inline-block',
+										'margin': '2px 0',
+										'padding': '.2em .6em .3em',
+										'font-size': '11px',
+										'font-weight': '700',
+										'line-height': '1',
+										'color': '#fff',
+										'text-align': 'center',
+										'white-space': 'nowrap',
+										'vertical-align': 'baseline',
+										'border-radius': '.25em',
+										'-webkit-font-smoothing': 'antialiased',
+										'-moz-osx-font-smoothing': 'grayscale'
+									}),
 								storageItem = localStorage.getItem(item.id);
 
 							if (storageItem) {
-								label
-									.html(storageItem)
-									.css('background-color', storageItem === 'master' ? '#5cb85c' : '#428bca');
+								label.html(storageItem);
+
+								if (storageItem === 'master') {
+									label.css('background-color', '#5cb85c');
+								} else {
+									label
+										.attr('target', '_blank')
+										.attr('href', 'https://github.inn.ru/4game-web/com.4game/compare/' + storageItem)
+										.css('background-color', '#428bca');
+								}
 							} else if (counts[item.id] && counts[item.id] >= retryCountUntilFail) {
 								label
 									.html('unknown branch')
